e0cd362997e9504b527f5bb5215fa5f7e9795c78
Brenda Wallace <brenda@wallace.net.nz>
2016-07-21 21:10:50 +0000

    Comment changes and tidy up.
    Signed-off-by: Brenda Wallace <brenda@wallace.net.nz>

--- whareora-wifi-gateway/whareora-wifi-gateway.ino
+++ whareora-wifi-gateway/whareora-wifi-gateway.ino
@@ -1,36 +1,4 @@
 /**
- * The MySensors Arduino library handles the wireless radio link and protocol
- * between your home built sensors/actuators and HA controller of choice.
- * The sensors forms a self healing radio network with optional repeaters. Each
- * repeater and gateway builds a routing tables in EEPROM which keeps track of the
- * network topology allowing messages to be routed to nodes.
- *
- * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
- * Copyright (C) 2013-2015 Sensnology AB
- * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
- *
- * Documentation: http://www.mysensors.org
- * Support Forum: http://forum.mysensors.org
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
- *
- *******************************
- *
- * REVISION HISTORY
- * Version 1.0 - Henrik Ekblad
- * 
- * DESCRIPTION
- * The ESP8266 MQTT gateway sends radio network (or locally attached sensors) data to your MQTT broker.
- * The node also listens to MY_MQTT_TOPIC_PREFIX and sends out those messages to the radio network
- *
- * LED purposes:
- * - To use the feature, uncomment WITH_LEDS_BLINKING in MyConfig.h
- * - RX (green) - blink fast on radio message recieved. In inclusion mode will blink fast only on presentation recieved
- * - TX (yellow) - blink fast on radio message transmitted. In inclusion mode will blink slowly
- * - ERR (red) - fast blink on error during transmission error or recieve crc error  
- * 
  * See http://www.mysensors.org/build/esp8266_gateway for wiring instructions.
  * nRF24L01+  ESP8266
  * VCC        VCC
@@ -48,13 +16,6 @@
  * - Connect CH_PD via 10K resistor to VCC
  * - Connect GPIO2 via 10K resistor to VCC
  * - Connect GPIO0 via 10K resistor to VCC, and via switch to GND ('bootload switch')
- * 
-  * Inclusion mode button:
- * - Connect GPIO5 via switch to GND ('inclusion switch')
- * 
- * Hardware SHA204 signing is currently not supported!
- *
- * Make sure to fill in your ssid and WiFi password below for ssid & pass.
  */
 
 #include <EEPROM.h>
@@ -100,31 +61,10 @@
 //#define MY_IP_SUBNET_ADDRESS 255,255,255,0
 
 
-// MQTT broker ip address.  
+// MQTT broker ip address.  (broker.wharesensors.co)
 #define MY_CONTROLLER_IP_ADDRESS 150, 242, 42, 42
-
 // The MQTT broker port to to open 
 #define MY_PORT 1883      
-
- /*
-// Flash leds on rx/tx/err
-#define MY_LEDS_BLINKING_FEATURE
-// Set blinking period
-#define MY_DEFAULT_LED_BLINK_PERIOD 300
-
-// Enable inclusion mode
-#define MY_INCLUSION_MODE_FEATURE
-// Enable Inclusion mode button on gateway
-#define MY_INCLUSION_BUTTON_FEATURE
-// Set inclusion mode duration (in seconds)
-#define MY_INCLUSION_MODE_DURATION 60 
-// Digital pin used for inclusion mode button
-#define MY_INCLUSION_MODE_BUTTON_PIN  3 
-
-#define MY_DEFAULT_ERR_LED_PIN 16  // Error led pin
-#define MY_DEFAULT_RX_LED_PIN  16  // Receive led pin
-#define MY_DEFAULT_TX_LED_PIN  16  // the PCB, on board LED
-*/
 
 #include <ESP8266WiFi.h>
 #include <MySensors.h>
@@ -140,6 +80,3 @@ void presentation() {
 void loop() {
   // Send locally attech sensors data here
 }
-
-
-
